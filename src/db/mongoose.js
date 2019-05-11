36e913eeaa02802c874dce4386c1e3c89224bf5f
mazdak <nazemi.works@gmail.com>
2019-05-11 02:16:11 -0700

    create 2 collection add create and read from rest api

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,21 +1,8 @@
 const mongoose = require('mongoose');
 
-
-
 //connect to db
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true
 });
 //........................................................
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-});
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 // we put main db file here to express can connect to it.
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 //...................................
 
 
@@ -14,18 +15,71 @@ const port = process.env.PORT || 3000;
 //to parse incoming json to us by post to obj
 app.use(express.json());
 
+
+//-----------------CREATE-------------------------
 app.post('/users', (req, res) => {
   const user = new User(req.body);
   user.save().then(() => {
+    res.status(201).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/users', (req, res) => {
+  User.find({}).then((users) => {
+    res.send(users);
+  }).catch((e) => {
+    res.status(500).send();
+  })
+})
+
+app.get('/users/:id', (req, res) => {
+  //mongoose change string id to obj by itself thnx mongooose.
+  const _id = req.params.id;
+  User.findById(_id).then((user) => {
+    if (!user) {
+      return res.status(404).send();
+    }
     res.send(user);
+  }).catch((e) => {
+    res.status(500).send();
+  })
+})
+
+
+//------------------READ---------------------
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+  task.save().then(() => {
+    res.status(201).send(task);
   }).catch((e) => {
     res.status(400).send(e);
   });
 });
 
 
+app.get('/tasks', (req, res) => {
+  Task.find({}).then((tasks) => {
+    res.send(tasks);
+  }).catch((e) => {
+    res.status(500).send();
+  })
+})
 
-
+app.get('/tasks/:id', (req, res) => {
+  //mongoose change string id to obj by itself thnx mongooose.
+  const _id = req.params.id;
+  Task.findById(_id).then((task) => {
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  }).catch((e) => {
+    res.status(500).send();
+  })
+})
 
 
 app.listen(port, () => {
